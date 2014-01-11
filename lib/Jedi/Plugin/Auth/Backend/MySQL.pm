8c110511ed67ede9e6d2142f0ec03127695164eb
celogeek <me@celogeek.com>
2014-01-11 15:27:03 +0100

    add MySQL Backend

---
+++ lib/Jedi/Plugin/Auth/Backend/MySQL.pm
@@ -0,0 +1,53 @@
+package Jedi::Plugin::Auth::Backend::MySQL;
+
+# ABSTRACT: MySQL backend
+
+use strict;
+use warnings;
+# VERSION
+
+use Carp;
+use Path::Class;
+use DBIx::Class::Migration;
+
+# connect / create / prepare db
+sub _prepare_database {
+  my ($db, $host, $port, $user, $password, $prefix) = @_;
+
+  my @connect_info = grep { defined } ("DBI:mysql:$db;host=$host;port=$port", $user, $password);
+
+  my $schema = Jedi::Plugin::Auth::DB->connect(@connect_info);
+
+  my $migration = DBIx::Class::Migration->new(
+    schema => $schema,
+  );
+
+  $migration->install_if_needed;
+  $migration->upgrade;
+
+  return $schema;
+}
+
+use Moo::Role;
+
+sub _build__jedi_auth_db {
+    my ($self) = @_;
+    my $class = ref $self;
+    my ($db, $host, $port, $user, $password, $prefix) = @{$self->jedi_config->{$class}{auth}{mysql}}{qw/
+      database_name hostname port user password prefix
+    /};
+
+    $db //= 'test';
+    $host //= 'localhost';
+    $port //= 3306;
+    $user //= 'root';
+
+    if (!defined $prefix) {
+      $prefix = lc($class);
+      $prefix =~ s/::/_/gx;
+    }
+
+    return _prepare_database($db, $host, $port, $user, $password, $prefix);
+}
+
+1;
